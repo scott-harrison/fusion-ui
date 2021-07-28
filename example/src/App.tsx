03d3921d6e2637321b6eeef7d325539f2e562565
Toruk <scotth142@googlemail.com>
2021-07-28 21:21:05 +0100

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'fusion-ui'
+import 'fusion-ui/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
