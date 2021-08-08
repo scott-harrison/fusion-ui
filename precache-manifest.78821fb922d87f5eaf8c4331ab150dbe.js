51bbcc8ab9bcd851ae3e204f2971e36f8e27a6e9
Toruk <scotth142@googlemail.com>
2021-08-08 20:57:12 +0100

    Updates

---
+++ precache-manifest.78821fb922d87f5eaf8c4331ab150dbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbea8faa9cf16a3ba7cb9cd5d203769",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c7cf91a6697f374ad0c9",
+    "url": "./static/css/main.13862bf3.chunk.css"
+  },
+  {
+    "revision": "661838e9b876d0a4c94b",
+    "url": "./static/js/2.b2e18bbb.chunk.js"
+  },
+  {
+    "revision": "df4964ae36e0e724ce02a12cce603843",
+    "url": "./static/js/2.b2e18bbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7cf91a6697f374ad0c9",
+    "url": "./static/js/main.4c16acd0.chunk.js"
+  },
+  {
+    "revision": "84dec63ed28438007933",
+    "url": "./static/js/runtime-main.e6ca8bb9.js"
+  }
+]);
