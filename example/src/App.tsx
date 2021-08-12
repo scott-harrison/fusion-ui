d3b478a773b1a99953c816b0f12686d51562db99
Toruk <scotth142@googlemail.com>
2021-08-12 10:19:14 +0100

    dependency upgrades

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
 import { GridContainer, GridRow, GridColumn } from 'fusion-ui'
 import Overview from './pages/Overview';
 import SideNav from './components/SideNav';
