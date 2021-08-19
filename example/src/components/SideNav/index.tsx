4f7f00c2f1058cbc9a4990c55b50cca2e7fd2eae
Toruk <scotth142@googlemail.com>
2021-08-19 22:33:56 +0100

    added title to sidebar in example

--- example/src/components/SideNav/index.tsx
+++ example/src/components/SideNav/index.tsx
@@ -11,11 +11,18 @@ const StickyNav = styled.div`
   overflow-y: auto;
 `;
 
+const Title = styled.div`
+    margin: 1.5rem 0 .2rem;
+    padding: .4rem .5rem;
+    color: rgba(0,0,0,0.65);
+`
+
 const SideNav = () => {
     return (
         <StickyNav className="fui-sidebar">
             <ul>
                 <li><a href="/components/overview">Components Overview</a></li>
+                <li><Title>General</Title></li>
                 <li><a href="/components/grid">Grid System</a></li>
                 <li><a href="/components/buttons">Buttons</a></li>
             </ul>
